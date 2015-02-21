782bcd97373ff07da930aeca49721af80893cc9d
Jose Diaz <jose.diaz@joedayz.pe>
2015-02-21 17:43:19 -0500

    2.09 update adapter

--- Senami/app/src/main/java/pe/joedayz/senami/ForecastFragment.java
+++ Senami/app/src/main/java/pe/joedayz/senami/ForecastFragment.java
@@ -35,6 +35,9 @@ import java.util.List;
  */
 public class ForecastFragment extends Fragment {
 
+
+    private ArrayAdapter<String> mForecastAdapter;
+
     public ForecastFragment() {
     }
 
@@ -83,7 +86,7 @@ public class ForecastFragment extends Fragment {
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
         // use it to populate the ListView it's attached to.
-        ArrayAdapter<String> forecastAdapter =
+        mForecastAdapter =
                 new ArrayAdapter<String>(
                         getActivity(), // The current context (this activity)
                         R.layout.list_item_forecast, // The name of the layout ID.
@@ -94,7 +97,7 @@ public class ForecastFragment extends Fragment {
 
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-        listView.setAdapter(forecastAdapter);
+        listView.setAdapter(mForecastAdapter);
 
         return rootView;
     }
@@ -283,6 +286,17 @@ public class ForecastFragment extends Fragment {
 
             // This will only happen if there was an error getting or parsing the forecast.
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+           if(result!=null){
+               mForecastAdapter.clear();
+               for(String dayForecastStr : result){
+                   mForecastAdapter.add(dayForecastStr);
+               }
+           }
+
         }
     }
 }
