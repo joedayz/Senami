416a4416444db71a06ff0b8028d25024527eae54
Jose Diaz <jose.diaz@joedayz.pe>
2015-03-07 16:05:06 -0500

    3.12 map intent

--- Senami/app/src/main/java/pe/joedayz/senami/MainActivity.java
+++ Senami/app/src/main/java/pe/joedayz/senami/MainActivity.java
@@ -1,13 +1,19 @@
 package pe.joedayz.senami;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +44,39 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+
+        if (id == R.id.action_map) {
+
+            openPreferredLocationInMap();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key)
+                , getString(R.string.pref_location_default));
+
+        //Usar el URI para mostrar una ubicacion en el map
+        //Intents implicitos
+        Uri geolocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geolocation);
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else{
+            Log.d(LOG_TAG, "No se pudo llamar a " + location + ", no hay app instalado");
+        }
+
+
+
     }
 
 
