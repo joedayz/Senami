33385ae4ccd86c07716ba4575a1be8c86d68ea39
Jose Diaz <jose.diaz@joedayz.pe>
2015-03-07 16:39:56 -0500

    3.13 sin detail fragment

--- Senami/app/src/main/java/pe/joedayz/senami/DetailActivity.java
+++ Senami/app/src/main/java/pe/joedayz/senami/DetailActivity.java
@@ -3,9 +3,13 @@ package pe.joedayz.senami;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,7 +55,14 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private static final String LOG_CAT = DetailFragment.class.getSimpleName();
+
+        private static final String FORECAST_SHARE_HASHTAG = "#SenamiApp";
+        private String mForecastStr;
+
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -62,12 +73,48 @@ public class DetailActivity extends ActionBarActivity {
             // The detail Activity called via intent.  Inspect the intent for forecast data.
             Intent intent = getActivity().getIntent();
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                 ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(forecastStr);
+                        .setText(mForecastStr);
             }
 
             return rootView;
         }
+
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            //inflate the menu; add items to action bar
+            inflater.inflate(R.menu.detailFragment, menu);
+
+            //recuperar el share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            //Get the provider
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            if(mShareActionProvider!=null){
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }else{
+                Log.d(LOG_CAT, "Share action provider is null?");
+            }
+
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent  = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+
+        }
     }
+
+
+
+
+
+
 }
